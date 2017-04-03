87af8508172fa0d61900fee6b4b3bea36e2273dc
Ciprian Miclaus <ciprianm@gmail.com>
2017-04-03 23:26:35 +0100

    .

---
+++ README.md
@@ -0,0 +1,2 @@
+# jserrlogger
+A javascript module that catches javascript errors and posts them back to a server to be logged and managed.
---
+++ jserrlogger.js
@@ -0,0 +1,97 @@
+/*
+jserrlogger.js v1.0.0
+
+Installs an onerror handler to catch javascript errors and logs them on a server
+via a JSONP call to an endpoint on a server to allow the server to log these errors.
+
+LICENSE: GNU Affero General Public License v3.0
+
+Permissions of this strongest copyleft license are conditioned on making available complete source code of licensed
+works and modifications, which include larger works using a licensed work, under the same license. Copyright and license 
+notices must be preserved. Contributors provide an express grant of patent rights. When a modified version is used to 
+provide a service over a network, the complete source code of the modified version must be made available.
+
+*/
+
+(function(wnd) {
+
+	var err_id = 0,
+		timeout = 3000,
+		url = "",
+		debugMode,
+		_addScript,
+		_log,
+		_consoleLog;
+
+	_consoleLog = function() {
+		if(debugMode && wnd.console && wnd.console.log) {
+			wnd.console.log.apply(0, arguments);
+		}
+	},
+
+	_addScript = function(src) {
+		try {
+			var script = document.createElement("script"),
+				script_id = "jserrlog" + err_id;
+			script.id = script_id
+			script.src = src;
+
+			document.getElementsByTagName("head")[0].appendChild(script);
+
+			wnd.setTimeout(function() {
+				try {
+					var script = document.getElementById(script_id);
+					document.getElementsByTagName("head")[0].removeChild(script);
+				}
+				catch (e) {
+					_consoleLog("internal error:", e);
+				}
+			}, timeout);
+		}
+		catch (e) {
+			_consoleLog("internal error:", e);
+		}
+	};
+
+	_log = function(err_msg, file, line_number) {
+		_consoleLog("_log:", err_msg, file, line_number);
+
+		// format the data for the request
+		var src = url + "?i=" + err_id;
+		src += "&sn=" + escape(document.URL);
+		src += "&fl=" + escape(file);
+		src += "&ln=" + line_number;
+		src += "&err=";
+		var m = escape(err_msg);
+		try {
+			m = m.substr(0, 2083 - src.length);
+		}
+		catch(e){}
+		src += m;
+		
+		_addScript(src);
+		err_id++;
+	};
+
+	wnd.jserrlogger = {
+		
+		install: function(url_, timeout_, debug_) {
+			url = url_||"";
+			timeout = timeout_||3000;
+			debugMode = debug_;
+
+			var prevOnError = wnd.onerror;
+			wnd.onerror = function(err_msg, file, line_number) {
+				_log(err_msg, file, line_number);
+				if(prevOnError) {
+					prevOnError(err_msg, file, line_number);
+				}
+			};
+		},
+
+		logErr: function(err_msg, file, line_number, col_number) {
+			_log(err_msg, file, line_number, col_number);
+		}
+	};
+
+})(window);
